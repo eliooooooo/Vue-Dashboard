c22bb8ccc6d20d4e25e4067de4070e0631ce6d38
eliooooooo <eliott.burkle@gmail.com>
2024-10-01 11:54:39 +0200

    Feat: init AlpineJS

--- src/main.js
+++ src/main.js
@@ -4,4 +4,8 @@ import router from './router'
 import './dist.css'
 import './style.css'
 
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
+
 createApp(App).use(router).mount('#app')
