2af3fbbb320c5a4172742b1e3a02686098ee6aa0
eliooooooo <eliott.burkle@gmail.com>
2024-10-02 17:55:56 +0200

    Feat: copy function on key + placeholder when no data

---
+++ src/components/SiteRow.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    site: {
+        type: Object,
+        required: true
+    }
+})
+
+const copyContent = (event) => {
+    navigator.clipboard.writeText(event.target.parentElement.previousElementSibling.textContent)
+    alert('Copied to clipboard')
+}
+
+const emits = defineEmits(['remove-site'])
+const removeSite = () => {
+    emits('remove-site', props.site.id)
+}
+</script>
+
+<template>
+    <tr class="border [&>td]:border [&>td]:p-1">
+        <td>{{ site.id }}</td>
+        <td>{{ site.name }}</td>
+        <td><a target="_blank" href="{{ site.url }}" class="flex flex-row gap-2 items-center">
+            {{ site.url }} 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+            </svg>
+        </a></td>
+        <td><div class="flex flex-row gap-2">
+            <p>{{ site.key }}</p><div @click="copyContent" class="block bg-gray-200 rounded-md cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy m-1.5" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+                </svg>
+            </div>
+        </div></td>
+        <td>
+            <router-link to="/sites/1">View</router-link>
+        </td>
+        <td class="flex flex-row gap-2 border-none">
+            <button class="btn small primary">Edit</button>
+            <button class="btn small secondary" @click="removeSite">Delete</button>
+        </td>
+    </tr>
+</template>
---
+++ src/components/Sites.vue
@@ -0,0 +1,70 @@
+<script setup>
+import SiteRow from './SiteRow.vue'
+import { ref, watch } from 'vue'
+
+const sites = ref([
+    { id: 1, name: 'Google', url: 'https://www.google.com', key: 'abcdefghijklmnopqrstuvwxyz' },
+    { id: 2, name: 'Facebook', url: 'https://www.facebook.com', key: 'abcdefghijklmnopqrstuvwxyz' }
+])
+
+const addSite = () => {
+    sites.value.push({ id: sites.value.length + 1, name: 'New Site', url: 'https://www.newsite.com', key: 'abcdefghijklmnopqrstuvwxyz' })
+}
+
+const removeSite = (id) => {
+    sites.value = sites.
+    value.filter(site => site.id !== id)
+}
+
+// watch(sites, (newSites) => {
+//     console.log('Sites:', newSites)
+// })
+</script>
+
+<template>
+    <div class="border border-dark p-4 rounded-md flex flex-col" x-data="{ openSites: true }">
+        <div class="flex flex-row justify-between items-center">
+            <div>
+                <h1>Websites</h1>
+                <p>Here are the websites you are tracking</p>
+            </div>
+            <div class="flex flex-row gap-3 items-center">
+                <a class="btn primary flex flex-row gap-2 items-center" x-on:click="openSites = true" @click="addSite()">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                    </svg>
+                    Add Site
+                </a>
+                <div class="cursor-pointer p-4 bg-primary-light rounded-md text-black" x-on:click="openSites = !openSites">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16" x-show="!openSites">
+                        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16" x-show="openSites">
+                        <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        <div class="w-full border-dark border-b pt-2" x-show="openSites" x-cloak transition></div>
+        <div x-show="openSites" x-cloak transition>
+            <table class="w-full mt-3" v-if="sites.length >= 1">
+                <thead class="bg-primary-light border border-primary-light">
+                    <tr class="[&>th]:text-left [&>th]:p-1 border-primary-light">
+                        <th>Id</th>
+                        <th>Site name</th>
+                        <th>URL</th>
+                        <th>Key</th>
+                        <th>Data</th>
+                        <th class="w-fit">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <SiteRow v-for="site in sites" :key="site.id" :site="site" @remove-site="removeSite"/>
+                </tbody>
+            </table>
+            <p class="mt-3" v-else>
+                No sites to display
+            </p>
+        </div>
+    </div>
+</template>
