d107e9e966714f1072ecda22a6a4ab55a6d0d60a
eliooooooo <eliott.burkle@gmail.com>
2024-10-31 20:13:40 +0100

    Feat: sitesheader stats

--- src/components/Sites.vue
+++ src/components/Sites.vue
@@ -1,11 +1,17 @@
 <script setup>
+import SitesNewVisits from './Sites/SitesNewVisits.vue';
+import SitesNumber from './Sites/SitesNumber.vue';
 import SitesTable from './Sites/SitesTable.vue';
-import SitesStats from './Sites/SitesStats.vue';
+import SitesVisits from './Sites/SitesVisits.vue';
 </script>
 
 <template>
     <div class="flex flex-col gap-4">
+        <div class="flex flex-row gap-4 [&>*]:w-full w-full">
+            <SitesNumber />
+            <SitesVisits />
+            <SitesNewVisits />
+        </div>
         <SitesTable />
-        <SitesStats />
     </div>
 </template>
