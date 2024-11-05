4ae3880c0f464691524643573cf4095a320749cb
eliooooooo <eliott.burkle@gmail.com>
2024-11-05 09:39:50 +0100

    Feat: add data to visits multiple chart

--- src/components/Sites/VisitsMultipleCharts.vue
+++ src/components/Sites/VisitsMultipleCharts.vue
@@ -5,6 +5,126 @@ import { Chart as ChartJS, Title, Tooltip, Legend } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend)
 
+const pagesDatas = ref ({
+    weekly: {
+        labels: ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300, 2100, 2400, 2600, 2700]
+    },
+    monthly: {
+        labels: ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing'],
+        data: [7600,8200,6100,6500,6800,7600,7400,7500,8000,8300]
+    },
+    yearly: {
+        labels: ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000]
+    }
+})
+
+const sourcesData = ref({
+    weekly: {
+        labels: ['social', 'news', 'print', 'ads', 'site', 'app'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['social', 'news', 'print', 'ads', 'site', 'app'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['social', 'news', 'print', 'ads', 'site', 'app'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const campaignData = ref({
+    weekly: {
+        labels: ['summer', 'winter', 'spring', 'fall', 'christmas', 'new year'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['summer', 'winter', 'spring', 'fall', 'christmas', 'new year'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['summer', 'winter', 'spring', 'fall', 'christmas', 'new year'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const mediumData = ref({
+    weekly: {
+        labels: ['organic', 'cpc', 'referral', 'email', 'social', 'direct'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['organic', 'cpc', 'referral', 'email', 'social', 'direct'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['organic', 'cpc', 'referral', 'email', 'social', 'direct'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const contentData = ref({
+    weekly: {
+        labels: ['blog', 'news', 'product', 'about', 'services', 'contact'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['blog', 'news', 'product', 'about', 'services', 'contact'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['blog', 'news', 'product', 'about', 'services', 'contact'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const termData = ref ({
+    weekly: {
+        labels: ['seo', 'sem', 'ppc', 'cpc', 'organic', 'referral'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['seo', 'sem', 'ppc', 'cpc', 'organic', 'referral'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['seo', 'sem', 'ppc', 'cpc', 'organic', 'referral'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const platformData = ref ({
+    weekly: {
+        labels: ['Chrome', 'Safari', 'Firefox', 'Opera', 'Bing', 'Other'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['Chrome', 'Safari', 'Firefox', 'Opera', 'Bing', 'Other'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['Chrome', 'Safari', 'Firefox', 'Opera', 'Bing', 'Other'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
+const languageData = ref ({
+    weekly: {
+        labels: ['English', 'French', 'Spanish', 'German', 'Italian', 'Other'],
+        data: [1500, 2000, 1800, 2200, 2500, 2300]
+    },
+    monthly: {
+        labels: ['English', 'French', 'Spanish', 'German', 'Italian', 'Other'],
+        data: [7600,8200,6100,6500,6800,7600]
+    },
+    yearly: {
+        labels: ['English', 'French', 'Spanish', 'German', 'Italian', 'Other'],
+        data: [30000, 35000, 40000, 45000, 50000, 55000]
+    }
+})
+
 const chartElement = ref([
     { name: 'page', value: 'Pages', id: 'pageRadio', checked: true },
     { name: 'source', value: 'Sources', id: 'sourceRadio', checked: false },
@@ -49,15 +169,30 @@ const updateChartData = () => {
     let labels = []
     let data = []
 
-    if (selectedPeriod.value === 'weekly') {
-        labels = ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing']
-        data = [1500, 2000, 1800, 2200, 2500, 2300, 2100, 2400, 2600, 2700]
-    } else if (selectedPeriod.value === 'monthly') {
-        labels = ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing']
-        data = [7600,8200,6100,6500,6800,7600,7400,7500,8000,8300]
-    } else if (selectedPeriod.value === "yearly") {
-        labels = ['index', 'contact', 'product', 'page', 'about', 'services', 'blog', 'portfolio', 'team', 'pricing']
-        data = [30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000]
+    if (selectedChartOption.value === 'page') {
+        labels = pagesDatas.value[selectedPeriod.value].labels
+        data = pagesDatas.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'source') {
+        labels = sourcesData.value[selectedPeriod.value].labels
+        data = sourcesData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'campaign') {
+        labels = campaignData.value[selectedPeriod.value].labels
+        data = campaignData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'medium') {
+        labels = mediumData.value[selectedPeriod.value].labels
+        data = mediumData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'content') {
+        labels = contentData.value[selectedPeriod.value].labels
+        data = contentData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'term') {
+        labels = termData.value[selectedPeriod.value].labels
+        data = termData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'platform') {
+        labels = platformData.value[selectedPeriod.value].labels
+        data = platformData.value[selectedPeriod.value].data
+    } else if (selectedChartOption.value === 'language') {
+        labels = languageData.value[selectedPeriod.value].labels
+        data = languageData.value[selectedPeriod.value].data
     }
 
     // Combiner labels et data
