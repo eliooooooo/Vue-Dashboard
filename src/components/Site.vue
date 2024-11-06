da54cb266dee292b4c4babf0ba9c3631a542e34c
eliooooooo <eliott.burkle@gmail.com>
2024-11-06 08:49:34 +0100

    Feat: cleanUp site template

--- src/components/Site.vue
+++ src/components/Site.vue
@@ -1,5 +1,4 @@
 <script setup>
-import SiteHeader from './Sites/SiteHeader.vue';
 import LocationChart from './Sites/LocationChart.vue';
 import SitesVisitsTable from './Sites/SitesVisitsTable.vue';
 import VisitsChart from './Sites/VisitsChart.vue';
@@ -8,7 +7,16 @@ import VisitsMultipleCharts from './Sites/VisitsMultipleCharts.vue';
 
 <template>
     <div class="grid grid-cols-12 gap-4">
-        <SiteHeader class="col-span-12" />
+        <div class="col-span-12 flex flex-row justify-between">
+            <div class="flex flex-col gap-2">
+                <h1>{ Website name }</h1>
+                <p>Site origin : { Origin link }</p>
+            </div>
+            <div class="flex flex-col gap-2">
+                <button class="btn primary small">Copy ApiKey</button>
+                <button class="btn secondary small">Delete </button>
+            </div>
+        </div>
         <VisitsChart class="col-span-8" />
         <LocationChart class="col-span-4" />
         <SitesVisitsTable class="col-span-12" :site-cell="false"/>
