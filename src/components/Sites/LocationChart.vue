c2f17f7e9ef6fd12f0d6cdae06a20da0c270644b
eliooooooo <eliott.burkle@gmail.com>
2024-11-10 22:38:11 +0100

    Rework: Location chart to vue composition

--- src/components/Sites/LocationChart.vue
+++ src/components/Sites/LocationChart.vue
@@ -1,34 +1,23 @@
-<script>
+<script setup>
 import { Doughnut } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from 'chart.js'
 import ContainerSimple from './../ContainerSimple.vue'
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement)
 
-export default {
-    name: 'LocationChart',
-    components: { Doughnut, ContainerSimple },
-    data() {
-        return {
-            chartData: {
-                labels: ['France', 'Allemagne', 'Italie', 'Others'],
-                datasets: [{
-                    data: [40, 20, 12, 2],
-                    backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
-                }]
-            },
-            chartOptions: {
-                responsive: true
-            }
-        }
-    }
-}
+const chartOptions = { responsive: true };
+const chartData = {
+    labels: ['France', 'Allemagne', 'Italie', 'Others'],
+    datasets: [{
+        data: [40, 20, 12, 2],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0']
+    }]
+};
+
 </script>
 
 <template>
     <ContainerSimple title="Location" desc="The location from where the users vitis your site" >
-        <div>
-            <Doughnut :options="chartOptions" :data="chartData"/>
-        </div>
+        <Doughnut :options="chartOptions" :data="chartData"/>
     </ContainerSimple>
 </template>
